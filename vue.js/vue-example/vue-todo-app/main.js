49a6301b101c6ed0823fb84a51dec47904724a41
dongwon kang <68678194+bluetuna1101@users.noreply.github.com>
2021-05-25 22:21:21 +0900

    vue 설치

--- vue.js/vue-example/vue-todo-app/main.js
+++ vue.js/vue-example/vue-todo-app/main.js
@@ -1,2 +1,14 @@
-// 
-console.log('hello webapck!web')
+// vue 라는 모듈을 가지고오겠다
+import Vue from 'vue'
+// main.js 진입점 
+// App.vue 연결할것임 (최상위 컴포넌트) 한번만 연결하면 되게 설정할것임
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  // 아래와 같이 축약 가능
+  // render (createElement) {
+  //   return createElement(App)
+  // },
+  render: h => h(App)
+})
