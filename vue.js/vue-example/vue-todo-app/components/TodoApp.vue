260c66f59053a950f2a7dd45bd08eb6663c17d26
dongwon kang <68678194+bluetuna1101@users.noreply.github.com>
2021-06-02 09:14:06 +0900

    vue

--- vue.js/vue-example/vue-todo-app/components/TodoApp.vue
+++ vue.js/vue-example/vue-todo-app/components/TodoApp.vue
@@ -7,8 +7,9 @@
 
 <script>
 // 노드 모듈에서 가져오는 모듈은 상단에
-// import low from "lowdb"
-// import LocalStorage from "lowdb/adapter/LocalStorage"
+
+import lowdb from 'lowdb'
+import LocalStorage from 'lowdb/adapters/LocalStorage'
 import TodoItem from "./TodoItem.vue"
 import TodoCreator from "./TodoCreator.vue"
 
@@ -17,26 +18,26 @@ export default {
     "todo-item": TodoItem,
     "todo-creator": TodoCreator,
   },
-  // data() {
-  //   return {
-  //     db: null,
-  //   }
-  // },
-  // created() {
-  //   this.initDB()
-  // },
-  // methods: {
-  //   initDB() {
-  //     const adapter = new LocalStorage("todo-app") //db 이름이 todoapp
-  //     this.db = low(adapter)
+  data() {
+    return {
+      db: null,
+    }
+  },
+  created() {
+    this.initDB()
+  },
+  methods: {
+    initDB() {
+      const adapter = new LocalStorage("todo-app") //db 이름이 todoapp
+      this.db = lowdb(adapter)
 
-  //     // Local DB 초기화
-  //     this.db
-  //       .defaults({
-  //         todos: [], // Collection
-  //       })
-  //       .write()
-  //   },
-  // },
+      // Local DB 초기화
+      this.db
+        .defaults({
+          todos: [], // Collection
+        })
+        .write()
+    },
+  },
 }
 </script>
