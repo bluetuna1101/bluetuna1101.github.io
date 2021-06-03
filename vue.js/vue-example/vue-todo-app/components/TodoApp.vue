bf02664a832e5db3eefb1a909edd60dce6ddf736
dongwon kang <68678194+bluetuna1101@users.noreply.github.com>
2021-06-04 01:31:56 +0900

    로컬 스토리지 세팅 및 데이터 입력 폼 작성

--- vue.js/vue-example/vue-todo-app/components/TodoApp.vue
+++ vue.js/vue-example/vue-todo-app/components/TodoApp.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="todo-app">
     <todo-item />
-    <todo-creator />
+    <!-- 자식인 todo-creator의 create-todo 이벤트가 실행될때 부모인 TodoApp 의 createTodo 메서드가 실행된다 -->
+    <todo-creator @create-todo="createTodo" />
   </div>
 </template>
 
@@ -9,6 +10,7 @@
 // 노드 모듈에서 가져오는 모듈은 상단에
 import low from 'lowdb'
 import LocalStorage from 'lowdb/adapters/LocalStorage'
+import cryptoRandomString from 'crypto-random-string'
 import TodoCreator from './TodoCreator'
 import TodoItem from './TodoItem'
 
@@ -39,6 +41,23 @@ export default {
         })
         .write()
     },
+    createTodo (title) {
+      const newTodo = {
+        id: cryptoRandomString({ length: 10}),
+        title,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        done: false
+      }
+
+      this.db
+      // lodash 문법을 사용
+      // todos 배열을 가져옴
+      .get('todos')
+      // 위에 선언한 nowTodo내용을 넣음
+      .push(newTodo)
+      .write() // lowdb (실제로 작성을 해라 !)
+    }
   },
 }
 </script>
--- vue.js/vue-example/vue-todo-app/components/TodoCreator.vue
+++ vue.js/vue-example/vue-todo-app/components/TodoCreator.vue
@@ -1,8 +1,39 @@
 <template>
-  <h1>Todo Creator</h1>
+  <div>
+    <button @click="createTodo">추가</button>
+    <!-- 작성 순서 : 바인딩 - 일반 - 이벤트 -->
+    <input
+      :value="title"
+      :placeholder="placeholder"
+      type="text"
+      @input="title = $event.target.value"
+      @keypress.enter="createTodo"
+      />
+  </div>
 </template>
 <script>
 export default {
-    name: 'TodoCreator',
+  name: 'TodoCreator',
+  data() {
+    return {
+      title: '',
+      placeholder: '할 일을 추가하세요!'
+    }
+  },
+  methods: {
+    createTodo () {
+      // 타이틀이 있고 앞뒤에 공백이 없는상태면 트루
+      const validatedTitle = this.title && this.title.trim()
+      if (!validatedTitle) {
+        alert('유효하지 않은 제목입니다!')
+        this.title = this.title.trim()
+        return
+      }
+
+      this.$emit('create-todo', this.title)
+      // 입력 후 타이틀 초기화 시켜줌
+      this.title = ''
+    }
+  }
 }
 </script>
