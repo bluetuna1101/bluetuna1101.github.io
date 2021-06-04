6b2862bbf42ca35e97fc69f25d73e0721b3f4a75
dongwon kang <68678194+bluetuna1101@users.noreply.github.com>
2021-06-04 22:35:24 +0900

    dayjs 설치 + 투두 앱 작업

--- vue.js/vue-example/vue-todo-app/components/TodoApp.vue
+++ vue.js/vue-example/vue-todo-app/components/TodoApp.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="todo-app">
-    <todo-item />
+    <todo-item 
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @update-todo="updateTodo"
+      @delete-todo="deleteTodo"
+    />
+    <hr>
     <!-- 자식인 todo-creator의 create-todo 이벤트가 실행될때 부모인 TodoApp 의 createTodo 메서드가 실행된다 -->
     <todo-creator @create-todo="createTodo" />
   </div>
@@ -11,6 +18,7 @@
 import low from 'lowdb'
 import LocalStorage from 'lowdb/adapters/LocalStorage'
 import cryptoRandomString from 'crypto-random-string'
+import _cloneDeep from 'lodash/cloneDeep'
 import TodoCreator from './TodoCreator'
 import TodoItem from './TodoItem'
 
@@ -34,12 +42,21 @@ export default {
       const adapter = new LocalStorage("todo-app") //db 이름이 todoapp
       this.db = low(adapter)
 
-      // Local DB 초기화
-      this.db
-        .defaults({
-          todos: [], // Collection
-        })
-        .write()
+      const hasTodos = this.db
+      .has('todos') // Collection name
+      .value() // has lodash 에서 db가 있는지 확인하는 메서드 .value -> 값을 뽑아냄
+
+      if(hasTodos) {
+      this.todos  = _cloneDeep(this.db.getState().todos) // getState 가져와라 _cloneDeep 참조까지 깊은복사해오기
+      } else {
+        // Local DB 초기화
+        this.db
+          .defaults({
+            todos: [], // Collection
+          })
+          .write()
+      }
+
     },
     createTodo (title) {
       const newTodo = {
@@ -57,7 +74,13 @@ export default {
       // 위에 선언한 nowTodo내용을 넣음
       .push(newTodo)
       .write() // lowdb (실제로 작성을 해라 !)
-    }
+    },
+    updateTodo() {
+      console.log('update todo')
+    },
+    deleteTodo() {
+      console.log('delete todo')
+    },
   },
 }
 </script>
--- vue.js/vue-example/vue-todo-app/components/TodoItem.vue
+++ vue.js/vue-example/vue-todo-app/components/TodoItem.vue
@@ -1,9 +1,69 @@
 <template>
   <div class="todo-item">
-    <h2>Todo Item</h2>
+    <input 
+      v-model="done"
+      type="checkbox"
+    >
+    <div class="item__title-wrap">
+      <div class="item__title">
+        {{ todo.title }}
+      </div>
+      <div class="item__date">
+        {{ date }}
+      </div>
+    </div>
+    <div class="item__actions">
+      <button @click="onEditMode">수정</button>
+      <button @click="deleteTodo">삭제</button>
+    </div>
   </div>
 </template>
 <script>
+import dayjs from 'dayjs'
+
 export default {
+  props: {
+    todo: Object
+  },
+  computed: {
+    // 객체리터럴 상태를 가지고옴
+    done: {
+      get () {
+        return this.todo.done
+      },
+      // done이 변경되었을때 분기시켜주는 셋
+      set (done) {
+        this.updateTodo({
+          done
+        })
+      }
+    },
+    // 날짜 나오게
+    date () {
+      const date = dayjs(this.todo.createdAt)
+      // 날짜 비교 - 같으면 트루 틀리면 펄스
+      const isSame = date.isSame(this.todo.updatedAt)
+      if (isSame) {
+        return date.format('YYYY년 MM월 DD일')
+      } else {
+        return `${date.format('YYYY년 MM월 DD일')} (edited)`
+      }
+
+    }
+  },
+  methods: {
+    onEditMode() {
+
+    },
+    offEditMode() {
+
+    },
+    updateTodo(value) {
+      this.$emit('update-todo', this.todo, value)
+    },
+    deleteTodo () {
+      this.$emit('delete-todo', this.todo)
+    }
+  }
 }
 </script>
