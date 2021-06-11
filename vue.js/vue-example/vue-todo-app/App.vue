7efbf9a52d5ca5d341e927081bcb8ec19fec2faa
dongwon kang <68678194+bluetuna1101@users.noreply.github.com>
2021-06-11 23:28:59 +0900

    home 버튼 추가

--- vue.js/vue-example/vue-todo-app/App.vue
+++ vue.js/vue-example/vue-todo-app/App.vue
@@ -1,4 +1,16 @@
 <template>
-  <!-- 각 페이지들의 출력되는 영역 설정 -->
-  <router-view />
+  <div>
+    <router-link to="/" class="to-home">
+    <i class="material-icons">home</i>
+    </router-link>
+    <!-- 각 페이지들의 출력되는 영역 설정 -->
+    <router-view />
+  </div>
 </template>
+
+<style lang="scss">
+// 실제 해당 페이지에서는 버튼 안보이게 처리
+  .to-home.router-link-exact-active {
+    display: none;
+  }
+</style>
