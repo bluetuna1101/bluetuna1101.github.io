ed93cb59bcbb4f43dfdf98e3ed6556bc4706f5b3
dongwon kang <68678194+bluetuna1101@users.noreply.github.com>
2021-06-20 19:22:36 +0900

    mutation actions 작성

--- vue.js/vue-example/vue-todo-app/store/todoApp.js
+++ vue.js/vue-example/vue-todo-app/store/todoApp.js
@@ -7,20 +7,31 @@ export default {
     todos: [],
   }),
   getters: {},
-  mutations: {},
+  mutations: {
+    assignDB(state, db) {
+      state.db = db
+    },
+    assignTodos(state, todos) {
+      state.todos = todos
+    }
+
+  },
   actions: {
     initDB({
-      state
+      state,
+      commit
     }) {
       const adapter = new LocalStorage("todo-app") //db 이름이 todoapp
-      state.db = low(adapter)
+      // state.db = low(adapter)
+      commit('assignDB', lowdb(adapter))
 
       const hasTodos = state.db
         .has('todos') // Collection name
         .value() // has lodash 에서 db가 있는지 확인하는 메서드 .value -> 값을 뽑아냄
 
       if (hasTodos) {
-        state.todos = _cloneDeep(state.db.getState().todos) // getState 가져와라 _cloneDeep 참조까지 깊은복사해오기
+        // state.todos = _cloneDeep(state.db.getState().todos) // getState 가져와라 _cloneDeep 참조까지 깊은복사해오기
+        commit('assignTodos', _cloneDeep(state.db.getState().todos))
       } else {
         // Local DB 초기화
         state.db
