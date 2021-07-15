8d7877b7aa1fc64d005e93b70e79719af600d7a9
dongwon kang <68678194+bluetuna1101@users.noreply.github.com>
2021-07-15 23:42:21 +0900

    router push 를 새창에서 열기로 변환하기

---
+++ vue.js/여러 기능들/router새창에서열기/router.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="open">open new window</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 기존 창 이동 rouet.push
+    open() {
+      this.$router.push({name: 'article-idx', params: {idx: item.idx}})
+    },
+    // resolve + window open으로 새창에서 열기
+    open() {
+      const detail = this.$router.resolve({name: 'article-idx', params: {idx: item.idx}})
+      window.open(detail.href, '_blank')
+    }
+  }
+}
+</script>
